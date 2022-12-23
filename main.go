93320b4d8ed8a3935d96071edafe7f26a9cf63f9
Matias <matias_haugum@hotmail.com>
2022-12-23 23:03:28 +0100

    Cleanup old comments

--- main.go
+++ main.go
@@ -20,42 +20,4 @@ func main() {
 	for {
 		select {}
 	}
-	/*fmt.Println(cardreader.DeviceInfo)
-	fmt.Println("new-code")
-	devicelist := hid.Enumerate(0, 0)
-	fmt.Println(devicelist)
-	kortleser, err := devicelist[0].Open()
-	if err != nil {
-		fmt.Println(err)
-	}
-	b := make([]byte, 3)
-	for {
-		_, err := kortleser.Read(b)
-		if err != nil {
-			fmt.Println(err)
-		}
-		if len(b) > 0 {
-			fmt.Println(b)
-		}
-	}
-	*/
-
-	//var testLeser hid.Device
-
-	//testLeser.DeviceInfo = io.EnumerateHID()
-	//fmt.Println(testLeser)
-	//testLeser.Open()
-
-	//publishchan := make(chan mqtt.Mqtt_message)
-	//go mqtt.StartMQTTserver(publishchan)
-
-	//message := mqtt.Mqtt_message{
-	//	Topic:   "input/card",
-	//	Message: "1239591",
-	//}
-
-	//for i := 0; i < 10; i++ {
-	//	publishchan <- message
-	//	time.Sleep(time.Second)
-	//}
 }
