018986f50961174e63ce7ac4d1102c227239b341
Matias <matias_haugum@hotmail.com>
2022-12-23 22:03:53 +0100

    Now reads buttons

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	io_button "godog/io_buttons"
 	io_card "godog/io_cardreader"
 	mqtt "godog/mqtt"
 )
@@ -9,11 +10,11 @@ import (
 func main() {
 
 	transmitchan := make(chan mqtt.Mqtt_message, 10)
-
 	io_card.CheckIfRoot()
 	HIDPath := io_card.CheckForCardreader()
 	cardreader := io_card.ConnectToCardReader(HIDPath)
 	go io_card.ReadFromCardReaderAndTransmit(*cardreader, transmitchan)
+	go io_button.PollButtonsAndTransmit(transmitchan)
 	for {
 		select {
 		case x := <-transmitchan:
